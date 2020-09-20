9213ddaf64c3ceb3517c20761010c2ba78585f31
dwf <dwf1023@163.com>
2020-09-20 21:22:24 +0800

    项目初始化

---
+++ src/main/java/com/noah/xinfeng/XinfengApplication.java
@@ -0,0 +1,13 @@
+package com.noah.xinfeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XinfengApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XinfengApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/noah/xinfeng/XinfengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noah.xinfeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XinfengApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
